eafdcd5d7d3caa10faf147837a83a381bc10e784
Dejorden <Dejorden@DESKTOP-6VJ23QR>
2023-04-20 19:07:49 +0200

    remove usings

--- PlayerFirst/BaseFixture.cs
+++ PlayerFirst/BaseFixture.cs
@@ -1,13 +1,6 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WebDriverManager.DriverConfigs.Impl;
-using WebDriverManager;
-using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 
 namespace PlayerFirst
